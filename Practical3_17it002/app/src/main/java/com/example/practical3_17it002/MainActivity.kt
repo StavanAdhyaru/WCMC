18ef8bec483abae14c6a42669d36da468f1ed740
StavanAdhyaru <46530959+StavanAdhyaru@users.noreply.github.com>
2019-12-26 10:29:26 +0530

    Add files via upload

---
+++ Practical3_17it002/app/src/main/java/com/example/practical3_17it002/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.example.practical3_17it002
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    
+}
